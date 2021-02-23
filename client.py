e7c9f6896b39f426b05d810a71aa7cab2f1a243c
Strat5 <61022957+Strat5@users.noreply.github.com>
2021-02-22 20:07:03 -0600

    Bug Fix

--- client.py
+++ client.py
@@ -2,16 +2,9 @@ import os
 import signal
 import requests
 import pyperclip
-confirmation='N'
-keyword = ''
-
-while confirmation=='N':
-	keyword = input('Enter the keyword: ')
-	print ('Received input was: "{}", is that correct?'.format(keyword))
-	confirmation = input('Y or N ').upper()
-
+keyword = input('Enter the keyword: ')
 url = 'https://fakesmsbot.herokuapp.com/'
 data = {'keyword':keyword}
 r = requests.post(url, json=data)
-print(r)
 pyperclip.copy(r.text)
+print(r)
