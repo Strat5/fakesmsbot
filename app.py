a11f29b99da3741c5e1f3312ea0d9bd9fd58458d
Strat5 <61022957+Strat5@users.noreply.github.com>
2021-02-22 18:17:57 -0600

    Client File Added

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-import os
+import os							#setup packages and variables
 import json
 from flask import Flask, request
 import requests
+app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+
+@app.route('/', methods=['POST'])	#clients feed this endpoint
 def webhook():
 	data = request.get_json()
 	keyword = data["keyword"]
 	log(keyword)
 	return "ok", 200
-
 
 def log(msg): 
 	print(str(msg))
---
+++ client.py
@@ -0,0 +1,2 @@
+keyword = input("Enter the keyword: ")
+print "Received input is : ", keyword
