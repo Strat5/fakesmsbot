1b53eb304c7c9b98e09f04ed263728dc240b781d
Strat5 <61022957+Strat5@users.noreply.github.com>
2021-02-22 19:24:21 -0600

    First Implementation of Functionality

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ import json
 from flask import Flask, request
 import requests
 app = Flask(__name__)
-
+import script
 
 @app.route('/', methods=['POST'])	#clients feed this endpoint
 def webhook():
 	data = request.get_json()
 	keyword = data["keyword"]
-	log(keyword)
-	return "ok", 200
+	log('The keyword "{}" was receieved.'.format(keyword))
+	message = script.keywords[keyword]
+	return message, 200
 
 def log(msg): 
 	print(str(msg))
