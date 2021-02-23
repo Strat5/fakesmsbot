5a68b9b7a1c126ac02fa867708d42c0c6259e48c
Strat5 <61022957+Strat5@users.noreply.github.com>
2021-02-22 20:42:22 -0600

    Bug Fix

--- client.py
+++ client.py
@@ -2,11 +2,9 @@ import os
 import signal
 import requests
 import pyperclip
-loop = true
-while loop:
-	keyword = input('Enter the keyword: ')
-	url = 'https://fakesmsbot.herokuapp.com/'
-	data = {'keyword':keyword}
-	r = requests.post(url, json=data)
-	pyperclip.copy(r.text)
-	print(r)
+keyword = input('Enter the keyword: ')
+url = 'https://fakesmsbot.herokuapp.com/'
+data = {'keyword':keyword}
+r = requests.post(url, json=data)
+pyperclip.copy(r.text)
+print(r)
