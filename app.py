d00486747f66f0c0140ab75168078884c48d773f
Strat5 <61022957+Strat5@users.noreply.github.com>
2021-02-22 20:55:26 -0600

    Final Bug Fix

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ import script
 @app.route('/', methods=['POST'])	#clients feed this endpoint
 def webhook():
 	data = request.get_json()
-	keyword = data["keyword"].upper()
+	keyword = data["keyword"]
 	log('The keyword "{}" was receieved.'.format(keyword))
-	if keyword in script.keywords:
+	if keyword == 'stop':
+		message = 'That is not a recognized keyword. Text STOP to end this program. Text TEEN to restart it.'
+	else if keyword.upper() in script.keywords:
 		message = 'SmokefreeTXT: ' + script.keywords[keyword]
 	else: 
 		message = 'That is not a recognized keyword. Text STOP to end this program. Text TEEN to restart it.'
