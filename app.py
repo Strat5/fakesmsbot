5bfe73a10cc863ef3ff20583be03e5eaf0613800
Strat5 <61022957+Strat5@users.noreply.github.com>
2021-02-22 18:05:54 -0600

    First Server Test

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import json
 from flask import Flask, request
 import requests
 
-@app.route('/', methods=['POST'])	#messages from GroupMe chats land here
+@app.route('/', methods=['POST'])
 def webhook():
 	data = request.get_json()
+	keyword = data['keyword']
+	log(('Keyword is: {}'.format(keyword))
+
+
+def log(msg): 
+	print(str(msg))
+	sys.stdout.flush()
