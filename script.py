349d71a3613d759c16f2842a3a0088191f09f516
Strat5 <61022957+Strat5@users.noreply.github.com>
2021-02-22 20:18:01 -0600

    Final Version

--- script.py
+++ script.py
@@ -1,4 +1,22 @@
 keywords = {
-	'UHOH' : 'If you have found yourself smoking a cigarette, text the keyword HELP for professional consulting. If you are experiencing headache or pains, text the keyword PAIN to receive medical advice.'
-	'PAIN' : 'If you have nausea or headaches, text SICK for a treatment. Reply BURN if you have been burned by a match or cigarette. If you swallowed a cigarette, you’re one down! Text HOPELESS to restart the program.'
+	'UHOH' : 'If you have found yourself smoking a cigarette, text the keyword HELP for professional consulting. If you are experiencing headache or pains, text the keyword PAIN to receive medical advice.',
+	'PAIN' : 'If you have nausea or headaches, text SICK for a treatment. Reply BURN if you have been burned by a match or cigarette. If you swallowed a cigarette, you’re one down! Text HOPELESS to restart the program.',
+	'BURN' : 'If you need immediate medical assistance, text SOS to call a professional team of cigarette (and occasionally water) burn specialists to evacuate your house and isolate the problem before giving you the treatment you deserve. If it is a minor burn, text RED to order and ship an ice cube.',
+	'RED' : 'Your ice cube has been shipped from Chandler, AZ. Reply SAFE to restart the program.',
+	'SOS' : 'A team has been dispatched to your location using the addressyou provided. Please remain calm and give them at least 10 minutes to arrive. Text STOP to cancel the request.',
+	'HELP' : 'You accidently started smoking. It’s O.K.! Reply with the keyword RANDOM if it was absent minded. Reply ME if it was intentional.',
+	'RANDOM' : 'It was an accident. Put the cigarette away, and have a glass of cold water. Record the day and time in your journal, and try to take your mind off of the incident. Reply TEEN to restart.',
+	'ME' : 'What do you expect us to do? If you cannot control your body, you need real help, And fast. You should be embarrassed with yourself. Text TEEN to restart the program. Text FAILURE to end your social life.',
+	'FAILURE' : 'Still here? You have spirit, congratulations. Maybe there is hope for you. You just have to have more determination. Text HOPE to restart your trial. \nText NO to waste our time further.',
+	'NO' : 'You are not in shape to succeed, Please reevaluate your choices. Reply STOP to end the program.',
+	'BOOST' : 'Reply STRONG for stronger cigarettes. Reply TIME for the longest lasting cigarettes.',
+	'STRONG' : 'You replied STRONG. Do you even take this program seriously? Text STOP to end your subscription immediately. Text HOPELESS to restart your trial.',
+	'TIME' : 'You replied TIME. Do you even take this program seriously? Text STOP to end yoursubscription immediately. Text HOPELESS to restart your trial.',
+	'WANT' : 'That is not specific enough. Are you craving a cigarette? If so, reply with the keyword NEED. If you want more information, visit https://www.smokefree.gov to stay updated.',
+	'NEED' : 'If you are struggling to refrain, have a cold glass of water. Try and take your mind off the issue and reply MORE. If you need a cigarette for a non-smoking reason, reply NEW for advice.',
+	'MORE' : 'How cold was the water? If it was under 50K, please consider going to a hospital Immediately. This is very serious. If it was, and you convulsing in pain, text, COLD right away. If you were burned, text BURN for help. If it was a normal temperature, congratulations. You may text TEEN to restart the program.',
+	'COLD' : 'You are a fraud and do not take this program seriously. If you were convulsing in pain, you would not be sending messages at the same time. Smokefree is a dedicated team of professionals, but we can only help you if you want to help yourself. Text HOPELESS to restart the program. Text NO to disobey.',
+	'BURN' : 'If you need immediate medical assistance, text SOS to call a professional team of cigarette (and occasionally water) burn specialists to evacuate your house and isolate the problem before giving you the treatment you deserve. If it is a minor burn, text RED to order and ship an ice cube.',
+	'NEW' : 'Why would you need a cigarette for a nonsmoking reason? Are you juggling or swallowing them? Do you need psychological help? If so, reply YES. If not, reply TEEN to restart the program or STOP to end the program.',
+	'YES' : 'We have reached out to respected psychiatrists in your area to help you. Please wait for an update in the next month. Reply TEEN to restart the program.'
 }
