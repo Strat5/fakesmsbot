638b4d2ded79d6528af9347611f10adc27b76c9c
Strat5 <61022957+Strat5@users.noreply.github.com>
2021-02-24 15:39:22 -0600

    Bug Fix #500

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def webhook():
 	if keyword == 'end':
 		message = 'That is not a recognized keyword. Text STOP to end this program. Text TEEN to restart it.'
 	elif keyword.upper() in script.keywords:
-		message = 'SmokefreeTXT: ' + script.keywords[keyword]
+		message = 'SmokefreeTXT: ' + script.keywords[keyword.upper()]
 	else: 
 		message = 'That is not a recognized keyword. Text STOP to end this program. Text TEEN to restart it.'
 	return message, 200
