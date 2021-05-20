c043c9b878c05c20752a68cfae3cfeb0b2e1e42a
Strat5 <61022957+Strat5@users.noreply.github.com>
2021-05-20 14:31:30 -0500

    WAR FILES ADDEDD

--- client.py
+++ client.py
@@ -2,7 +2,20 @@ import os
 import signal
 import requests
 import pyperclip
+import webbrowser
+
 keyword = input('Enter the keyword: ')
+if(keyword == 'WAR_FILES'):
+	keyword = input('Please enter the Document ID: ')
+	if(keyword == 'backup_communication'):
+		url = 'https://docs.google.com/document/d/1_CUCJjSI2kxA984FsZSq4gLT4Xr1r8XMWQcuXHLNX60/edit?usp=sharing'
+		webbrowser.open_new(url)
+	elif(keyword == 'declaration_of_war'):
+		keyword = input("Are you sure you want to view this? It may be too intimidating for you.")
+		url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+		webbrowser.open_new(url)
+	exit()
+
 url = 'https://fakesmsbot.herokuapp.com/'
 data = {'keyword':keyword}
 r = requests.post(url, json=data)
